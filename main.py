abd02471f7d5a2d2084aa2c83b261bfab42e077f
cact74 <g4886640@gmail.com>
2026-05-14 12:02:40 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -27,11 +27,47 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+
+
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and direction != "down":
+                direction = "up"
+            
+            if event.key == pygame.K_DOWN and direction != "up":
+                direction = "down"
+
+            if event.key == pygame.K_LEFT and direction != "right":
+                direction = "left" 
+
+            if event.key == pygame.K_RIGHT and direction != "left":
+                direction = "right"
+
+    if game_state =="PLAY":
+        new_head =list(snake[0])
+        if direction == "up":
+            new_head[1] -= SIZE
+        if direction == "down":
+            new_head[1] += SIZE
+        if direction == "left":
+            new_head[0] -= SIZE
+        if direction == "right":
+            new_head[0] += SIZE
+        snake.insert(0, new_head)
+
+
+        if snake[0] == apple:
+            score += 1
+            apple = create_apple()
+        else:
+            snake.pop()
+
+
+
             
     draw_game(screen, snake, apple, score)
 
     pygame.display.update()
     clock.tick(FPS)
-
 
 
